cc5258e6a4f00f4d3b5074d6e42e558684f97901
Nantawat Chairat <nantawat@opsta.co.th>
2021-10-22 18:27:56 +0700

    update

--- src/app.py
+++ src/app.py
@@ -1,12 +1,21 @@
 from markupsafe import escape
 from flask import Flask
+from fluent import sender
+from fluent import event
 
 app = Flask(__name__)
 
+logger = sender.FluentSender('app', host='fluentd', port=24224)
+
 @app.route("/")
 def hello_world():
+    logger.emit('follow', {'from': 'userA', 'to': 'userB'})
+    logger.close()
+
     return "<p>Hello, World!</p>"
 
 @app.route("/<name>")
 def hello(name):
+    logger.emit('follow', {'from': 'userA', 'to': 'userB'})
+    logger.close()
     return f"Hello, {escape(name)}!"
